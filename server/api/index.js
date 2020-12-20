f8ca29dac6be401dc25a169cb420fe179eaad362
Juan Velazquez <juandvelaz@gmail.com>
2020-12-19 23:26:13 -0600

    Customize Route error handling middleware

--- server/api/index.js
+++ server/api/index.js
@@ -5,7 +5,7 @@ router.use('/test', require('./routes/test'));
 
 // Route error handling middleware
 router.use((req, res, next) => {
-  const error = new Error('Error 404: Page not found');
+  const error = new Error('404 - Not Found');
   error.status = 404;
   next(error);
 });
